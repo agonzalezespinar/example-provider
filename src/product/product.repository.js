5b0626d6a3f71a195cbefd9d907f9049b288c379
Alvaro Gonzalez Espinar <agonzalez@evidation.com>
2023-03-13 09:59:07 -0400

    Adding chnages to support the new color field on the items

--- src/product/product.repository.js
+++ src/product/product.repository.js
@@ -1,17 +1,28 @@
-const Product = require('./product');
+const Product = require("./product");
 
 class ProductRepository {
-
     constructor() {
         this.products = new Map([
-            ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
-            ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")],
-            ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2")],
+            ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", "green")],
+            [
+                "10",
+                new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "blue"),
+            ],
+            [
+                "11",
+                new Product(
+                    "11",
+                    "PERSONAL_LOAN",
+                    "MyFlexiPay",
+                    "v2",
+                    "yellow"
+                ),
+            ],
         ]);
     }
 
     async fetchAll() {
-        return [...this.products.values()]
+        return [...this.products.values()];
     }
 
     async getById(id) {
